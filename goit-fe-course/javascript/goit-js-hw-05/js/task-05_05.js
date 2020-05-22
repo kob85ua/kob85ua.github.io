8b18875b4869690ed1746e8d3c1d059427f3003f
kob85ua <kob85ua@gmail.com>
2020-05-22 14:37:30 +0300

    Update task-05_05.js

--- goit-fe-course/javascript/goit-js-hw-05/js/task-05_05.js
+++ goit-fe-course/javascript/goit-js-hw-05/js/task-05_05.js
@@ -35,22 +35,30 @@ class Car {
 
 	accelerate(value) {
 		this.speed += value;
+		if (this.speed > this.maxSpeed) {
+			this.speed = this.maxSpeed;
+		}
+		return this.speed;
 	}
 
 	decelerate(value) {
-		if (this.speed >= 0) {
-			this.speed -= value;
+		this.speed -= value;
+		if (this.speed < 0) {
+			this.speed = 0;
 		}
 		return this.speed;
 	}
 
 	drive(hours) {
-		this.distance = this.distance + hours * this.speed;
+		if (this.isOn === true) {
+			this.distance = this.distance + hours * this.speed;
+		}
+		return this.distance;
 	}
 }
-
+console.dir(Car);
 const mustang = new Car({ maxSpeed: 200, price: 2000 });
-
+console.dir(mustang);
 mustang.turnOn();
 mustang.accelerate(50);
 mustang.drive(2);
@@ -62,7 +70,6 @@ mustang.drive(1);
 mustang.turnOff();
 
 Car.getSpecs(mustang);
-
 
 console.log(mustang.price);
 mustang.price = 4000;
