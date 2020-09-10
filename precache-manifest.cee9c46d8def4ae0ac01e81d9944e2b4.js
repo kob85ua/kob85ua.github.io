0b46d374acf76d51530bb040f677abcd8265f69d
M.Kobeliev <kob85ua@gmail.com>
2020-09-10 10:31:25 +0300

    Updates

---
+++ precache-manifest.cee9c46d8def4ae0ac01e81d9944e2b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3754624d9dfec7ce2ee03bfab8702897",
+    "url": "/ghpages-deployment/index.html"
+  },
+  {
+    "revision": "66d46caf9f52a94e4f07",
+    "url": "/ghpages-deployment/static/js/2.67f8c7c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ghpages-deployment/static/js/2.67f8c7c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29612103e66bf8612899",
+    "url": "/ghpages-deployment/static/js/main.e7f15675.chunk.js"
+  },
+  {
+    "revision": "d718748243c431848703",
+    "url": "/ghpages-deployment/static/js/runtime-main.06be8fc5.js"
+  }
+]);
